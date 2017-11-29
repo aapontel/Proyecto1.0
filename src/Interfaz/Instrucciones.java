7b37c0394f67bd9fd169b80b7588b44f5cee34d0
Carlos <Carlos@192.168.1.22>
2017-11-29 00:06:19 -0500

    Merge origin/master
    Conflicts:
    	src/parcial/Programacion.java

--- src/Interfaz/Instrucciones.java
+++ src/Interfaz/Instrucciones.java
@@ -25,7 +25,7 @@ public class Instrucciones extends javax.swing.JFrame {
         jTextArea1.setEditable(false);
         jTextArea1.setBackground(new java.awt.Color(204, 204, 255));
         jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("Freestyle Script", 0, 24)); // NOI18N
+        jTextArea1.setFont(new java.awt.Font("High Tower Text", 0, 14)); // NOI18N
         jTextArea1.setRows(5);
         jTextArea1.setText("      Buen día Director,\n\n Soy el Secretario de dirección de la Facultad, y estoy aquí para \n guiaros en los primeros pasos de la dirección académica.\n\n Cada turno llegan cinco refuerzos, que pueden aumentar por los \n bonos específicos de las facultades controladas. \n Estos deben ser colocados en su totalidad en los edificios aliados.\n\n Luego comienza la fase de Transferencia y Ataque, en que se \n podrán mover las tropas a territorios conectados directamente, que,\n en caso de estar controlados por el enemigo, entrarán en batalla.\n\n Recuerde Director proteger sus edificios, y que SIEMPRE, \n al menos un hombre debe permanecer presente en cada edificio.");
         jTextArea1.setMargin(new java.awt.Insets(3, 3, 3, 3));
